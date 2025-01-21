438365f660ab34d7f2b95a36640103b28ad2e69f
TobiasReaper47 <tobiasreaper273@gmail.com>
2025-01-21 10:34:07 +0100

    Refactoring

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,11 +5,15 @@ public class Cat {
     private String firstName;
     private String furColor;
     private int age;
+    private boolean isFemale;
 
-    public Cat(String firstName, String furColor, int age) {
+    private int counter;
+
+    public Cat(String firstName, String furColor, int age, boolean isFemale) {
         this.firstName = firstName;
         this.furColor = furColor;
         this.age = age;
+        this.isFemale = isFemale;
     }
 
     /** Getter */
@@ -25,11 +29,31 @@ public class Cat {
     }
 
     public String getAge() {
-        return Integer.toString(age);
+        if (isFemale) {
+            return checkCompliance();             
+        } else {
+            return Integer.toString(age);
+        }
+      
     }
 
-    
-  
+     
+    private String checkCompliance(){
+
+        counter = 3;
+
+        switch (counter) {
+            case 1: 
+                return "This is an inappropriate question!"; // 1st time            
+            case 2: 
+                return "I`ve told you once!";   // 2nd time                
+            case 3:
+               return "Talk to the hand";   // 3rd time
+            default:
+                return "&%/&!?";    // 4th time
+            } 
+        }
+}    
 
 
-}
+
